9ff615a456e2d5a8d50e4665818a8cc4ce63041b
Shidesin <shidesin@gmail.com>
2020-07-25 02:18:17 +0300

    first_HW_Ignat_IT_Incubator

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import styles from './Message.module.css'
 
-function Message() {
+type PropsType = {
+    avatar: string,
+    name: string,
+    message: string
+    time: string
+}
+
+function Message(props: PropsType) {
     return (
         <div>
 
+            <div className={styles.messages_block}>
+                <div className={styles.avatar_block}><img src={props.avatar} alt="avatar"/></div>
+                <div className={styles.item}>
+                    <div className={styles.name_block}>{props.name}</div>
+                    <div className={styles.text_time}>
+                        <div className={styles.message_block}>{props.message}</div>
+                        <div className={styles.time_block}>{props.time}</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
